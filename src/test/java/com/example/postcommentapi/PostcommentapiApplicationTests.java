d971329560c998102b8ce4ecdc7f732a194e4e0a
Rodrigo Alexandre <rodrigo11.vgp@gmail.com>
2026-04-21 22:41:51 -0300

    Initial commit

---
+++ src/test/java/com/example/postcommentapi/PostcommentapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.postcommentapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostcommentapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
