cbcb6c4af302e1f1d79e3db544760ce177083c7c
Rodrigo Alexandre <rodrigo11.vgp@gmail.com>
2026-04-24 16:11:02 -0300

    feat: add PostService and PostResource for post retrieval by ID

---
+++ src/main/java/com/example/postcommentapi/services/PostService.java
@@ -0,0 +1,25 @@
+package com.example.postcommentapi.services;
+
+import com.example.postcommentapi.domain.Post;
+import com.example.postcommentapi.repository.PostRepository;
+import com.example.postcommentapi.services.exception.ObjectNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class PostService {
+    @Autowired
+    private PostRepository repo;
+
+    public Post findById(String id) {
+        Post user = repo.findById(id).orElse(null);
+
+        if (user == null) {
+            throw new ObjectNotFoundException("Post not found. Id: " + id);
+        }
+        return user;
+    }
+
+}
