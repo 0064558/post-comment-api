7ba733d8f2e72f92eea70753ac0caa4803085620
Rodrigo Alexandre <rodrigo11.vgp@gmail.com>
2026-04-24 17:11:27 -0300

    feat: add searchByTitle method with @Query

--- src/main/java/com/example/postcommentapi/repository/PostRepository.java
+++ src/main/java/com/example/postcommentapi/repository/PostRepository.java
@@ -1,12 +1,16 @@
 package com.example.postcommentapi.repository;
 
 import com.example.postcommentapi.domain.Post;
-import com.example.postcommentapi.domain.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 
 import java.util.List;
 
 // Interface de acesso a dados PostRepository, responsável por realizar operações de CRUD (Create, Read, Update, Delete) na coleção de posts do MongoDB. Ela estende a interface MongoRepository, que fornece métodos prontos para manipulação dos dados, como salvar, buscar por ID, deletar, etc. O tipo de entidade é Post e o tipo do ID é String.
 public interface PostRepository extends MongoRepository<Post, String> {
+
+    @Query("{ 'title': { $regex: ?0, $options: 'i' } }")
+    List<Post> searchByTitle(String text);
+
     List<Post> findByTitleContainingIgnoreCase(String text);
 }
